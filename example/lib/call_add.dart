05995f6d08e686b1844c06403e9ab5b2bff75fc1
Siprix Dev <developers@siprix-voip.com>
2024-11-23 19:32:21 +0200

    Added list of recent calls (CDRs) on the "Add Call" page

--- example/lib/call_add.dart
+++ example/lib/call_add.dart
@@ -3,6 +3,9 @@ import 'package:provider/provider.dart';
 
 import 'package:siprix_voip_sdk/accounts_model.dart';
 import 'package:siprix_voip_sdk/calls_model.dart';
+import 'package:siprix_voip_sdk/cdrs_model.dart';
+
+enum CdrAction {delete, deleteAll}
 
 ////////////////////////////////////////////////////////////////////////////////////////
 //CallAddPage - allows enter destination number and source account. Used for starting new outgoing calls
@@ -16,9 +19,10 @@ class CallAddPage extends StatefulWidget {
   State<CallAddPage> createState() => _CallAddPageState();
 }
 
-class _CallAddPageState extends State<CallAddPage> {  
-  final _phoneNumbCtrl = TextEditingController();  
+class _CallAddPageState extends State<CallAddPage> {
+  final _phoneNumbCtrl = TextEditingController();
   String _errText="";
+  int _selCdrRowIdx=0;
 
   @override
   Widget build(BuildContext context) {
@@ -37,29 +41,24 @@ class _CallAddPageState extends State<CallAddPage> {
 
   Widget _buildBody(AccountsModel accounts) {
     return 
-      Container(padding: const EdgeInsets.all(10.0),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            _buildAccountsMenu(accounts),
-            const SizedBox(height: 15,),
-            _buildPhoneNumberField(),
-            const SizedBox(height: 15),
-            Wrap(alignment: WrapAlignment.center, spacing: 25, children:[
-              ElevatedButton(onPressed: _inviteAudio, child: const Icon(Icons.add_call)),
-              ElevatedButton(onPressed: _inviteVideo, child: const Icon(Icons.video_call_outlined))
-            ]),
-            const Spacer(),
+      Column(crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(color: Theme.of(context).dialogBackgroundColor, padding: const EdgeInsets.fromLTRB(10,0,10,5), 
+            child: Column(children: [
+              _buildAccountsMenu(accounts),
+              const SizedBox(height: 5),
+              _buildPhoneNumberField(),
+            ])),
+          Expanded(child: buildCdrsList()),
+          if(_errText.isNotEmpty)
             Text(_errText, style: const TextStyle(color: Colors.red))
-          ]
-        ),
-      );
+        ]);
   }
 
   Widget _buildAccountsMenu(AccountsModel accounts) {
-    return ButtonTheme(alignedDropdown: true, child: DropdownButtonFormField<int>(
+    return ButtonTheme(child: DropdownButtonFormField<int>(
       decoration: const InputDecoration(border: UnderlineInputBorder(), 
-        labelText: 'Select account:', 
-        contentPadding: EdgeInsets.all(0)),
+        labelText: 'Select account:', ),
       value: accounts.selAccountId,
       onChanged: (int? accId) {  accounts.setSelectedAccountById(accId!); },
       items: List.generate(accounts.length, (index) => accMenuItem(accounts[index], index)),
@@ -72,14 +71,98 @@ class _CallAddPageState extends State<CallAddPage> {
 
   Widget _buildPhoneNumberField() {
     return TextFormField(
-      style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600),
-      decoration: const InputDecoration(
-        border: UnderlineInputBorder(),
+      style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+      decoration: InputDecoration(
+        border: const UnderlineInputBorder(),
         labelText: 'Enter phone number',
-        contentPadding: EdgeInsets.all(0),
+        //contentPadding: const EdgeInsets.all(0),
+        suffixIcon: Wrap(alignment: WrapAlignment.center, spacing: 10, children:[
+            FilledButton(onPressed: _inviteAudio, child: const Icon(Icons.add_call)),
+            OutlinedButton(onPressed: _inviteVideo, child: const Icon(Icons.video_call_outlined))
+            ]),
       ),
       controller: _phoneNumbCtrl,
     );
+  }
+
+  Widget buildCdrsList() {
+    final cdrs = context.watch<CdrsModel>();
+    return ListView.separated(
+      scrollDirection: Axis.vertical,
+      itemCount: cdrs.length,
+      itemBuilder: (BuildContext context, int index) {
+        CdrModel cdr = cdrs[index];
+        return ListTile(
+          selected: (_selCdrRowIdx == index),
+          selectedColor: Colors.black,
+          selectedTileColor: Theme.of(context).secondaryHeaderColor,
+          //contentPadding:const EdgeInsets.fromLTRB(0, 0, 10, 0),
+          leading: _getCdrIcon(cdr),
+          title: _getCdrTitle(cdr),
+          subtitle: _getCdrSubTitle(cdr, (_selCdrRowIdx == index)),
+          trailing: _getCdrRowTrailing(cdr, index),
+          dense: true,
+          onTap: () { 
+              setState(() {
+                context.read<AccountsModel>().setSelectedAccountByUri(cdr.accUri);
+                _phoneNumbCtrl.text = cdr.remoteExt;
+                _selCdrRowIdx = index;
+              });
+            }
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(height: 0,),
+    );
+  }
+
+  Icon _getCdrIcon(CdrModel cdr) {
+    if(cdr.incoming) {
+      return cdr.connected ? const Icon(Icons.call_received_rounded, color: Colors.green,)
+                           : const Icon(Icons.call_missed_rounded, color: Colors.red);
+    }
+    else {
+      return cdr.connected ? const Icon(Icons.call_made_rounded, color: Colors.lightGreen)
+                           : const Icon(Icons.call_missed_outgoing_rounded, color: Colors.orange);
+    }
+  }
+
+  Widget _getCdrTitle(CdrModel cdr) {
+    return 
+      Text(cdr.displName.isEmpty ? cdr.remoteExt : "${cdr.displName} (${cdr.remoteExt})", 
+        style: Theme.of(context).textTheme.titleSmall);
+  }
+
+  Widget _getCdrSubTitle(CdrModel cdr, bool isSelected) {
+    Text accText = Text(cdr.accUri, style: const TextStyle(fontSize: 12.0, fontStyle: FontStyle.italic, color: Colors.grey));
+    if(!isSelected) { return accText;  }
+    else {
+      return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        accText,
+        Wrap(spacing:5, children: [
+          Text(cdr.madeAtDate), 
+          if(cdr.connected) Text("Duration: ${cdr.duration}"),
+          if(cdr.statusCode!=0) Text("Status code: ${cdr.statusCode}"),
+          if(cdr.hasVideo) const Icon(Icons.videocam_outlined, color: Colors.grey, size:18),
+        ])
+      ]);
+    }
+  }
+
+  Widget _getCdrRowTrailing(CdrModel cdr, int index) {
+    return 
+        PopupMenuButton<CdrAction>(
+          onSelected: (CdrAction action) { _onCdrMenuAction(action, index); },
+          itemBuilder: (BuildContext context) => <PopupMenuEntry<CdrAction>>[
+            const PopupMenuItem<CdrAction>(
+              value: CdrAction.delete,
+              child: Wrap(spacing:5, children:[Icon(Icons.delete), Text("Delete"),])
+            ),
+          ]);
+  }
+
+  void _onCdrMenuAction(CdrAction action, int index) {
+    final cdrs = context.read<CdrsModel>();
+    cdrs.remove(index);
   }
 
   void _inviteVideo() => _invite(true);
